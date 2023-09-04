9916a75e5d556f07ab1fa7c12f1010df6a8e7939
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-04 17:19:38 +0200

    Attribute binding

--- tutorial-1/app.js
+++ tutorial-1/app.js
@@ -1,11 +1,24 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "https://www.google.com/",
       showBooks: true,
       books: [
-        { title: "The Final Empire", author: "Brandon Sanderson" },
-        { title: "The Final Empire 2", author: "Brandon Sanderson" },
-        { title: "The Final Empire 3", author: "Brandon Sanderson" },
+        {
+          title: "The Final Empire",
+          author: "Brandon Sanderson",
+          image: "https://randomuser.me/api/portraits/thumb/men/1.jpg",
+        },
+        {
+          title: "The Final Empire 2",
+          author: "Brandon Sanderson",
+          image: "https://randomuser.me/api/portraits/thumb/men/2.jpg",
+        },
+        {
+          title: "The Final Empire 3",
+          author: "Brandon Sanderson",
+          image: "https://randomuser.me/api/portraits/thumb/men/3.jpg",
+        },
       ],
       x: 0,
       y: 0,
