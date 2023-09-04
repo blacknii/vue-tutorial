b8010d74bb41cc4c7e7c7459938ed71ea091d842
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-04 18:07:30 +0200

    Dynamic classes

--- tutorial-1/app.js
+++ tutorial-1/app.js
@@ -5,19 +5,25 @@ const app = Vue.createApp({
       showBooks: true,
       books: [
         {
+          id: 0,
           title: "The Final Empire",
           author: "Brandon Sanderson",
           image: "https://randomuser.me/api/portraits/thumb/men/1.jpg",
+          isFav: true,
         },
         {
+          id: 1,
           title: "The Final Empire 2",
           author: "Brandon Sanderson",
           image: "https://randomuser.me/api/portraits/thumb/men/2.jpg",
+          isFav: false,
         },
         {
+          id: 2,
           title: "The Final Empire 3",
           author: "Brandon Sanderson",
           image: "https://randomuser.me/api/portraits/thumb/men/3.jpg",
+          isFav: true,
         },
       ],
       x: 0,
@@ -34,6 +40,10 @@ const app = Vue.createApp({
     handleMousemove(e) {
       this.x = e.offsetX;
       this.y = e.offsetY;
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav;
+      console.log(book);
     },
   },
 });
