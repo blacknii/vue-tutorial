1ab38130f1b875b8b69ccbaa8dbfb4b1c77f8db0
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-04 16:56:08 +0200

    Other mouse events

--- tutorial-1/app.js
+++ tutorial-1/app.js
@@ -5,11 +5,20 @@ const app = Vue.createApp({
       title: "The Final Empire",
       author: "Brandon Sanderson",
       age: 45,
+      x: 0,
+      y: 0,
     };
   },
   methods: {
     toggleShowBox() {
       this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, number) {
+      console.log(e.type, number);
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
     },
   },
 });
