4ff6026626dc59fad1b2c62bf319711c724e0263
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-07 11:36:16 +0200

    Vue Router Basics

---
+++ tutorial-5/src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est ducimus
+      sequi ut deleniti deserunt accusantium illum iusto ipsa possimus quos rem
+      nostrum, fugit ullam doloremque ratione ad. Unde molestiae obcaecati
+      asperiores sunt ab impedit deleniti totam aliquam facere dicta similique
+      vel, blanditiis tenetur excepturi perferendis incidunt? Alias ab facilis
+      officia dolore minus error, molestiae provident esse, magnam molestias
+      fuga veniam distinctio ea officiis suscipit et aliquid id velit
+      consequatur harum asperiores eveniet sapiente itaque quae. Vitae,
+      repellendus nihil. Vel non libero veniam. Sed deleniti libero alias?
+      Mollitia quidem illo, repudiandae qui quas velit. Accusantium, rerum? Hic
+      ratione illo cum eligendi.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est ducimus
+      sequi ut deleniti deserunt accusantium illum iusto ipsa possimus quos rem
+      nostrum, fugit ullam doloremque ratione ad. Unde molestiae obcaecati
+      asperiores sunt ab impedit deleniti totam aliquam facere dicta similique
+      vel, blanditiis tenetur excepturi perferendis incidunt? Alias ab facilis
+      officia dolore minus error, molestiae provident esse, magnam molestias
+      fuga veniam distinctio ea officiis suscipit et aliquid id velit
+      consequatur harum asperiores eveniet sapiente itaque quae. Vitae,
+      repellendus nihil. Vel non libero veniam. Sed deleniti libero alias?
+      Mollitia quidem illo, repudiandae qui quas velit. Accusantium, rerum? Hic
+      ratione illo cum eligendi.
+    </p>
+  </div>
+</template>
---
+++ tutorial-5/src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <h1>This is an home page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque deleniti
+      aliquid eius atque unde, accusantium placeat, reiciendis quam delectus,
+      expedita nostrum quis. Illum optio, velit modi dolor blanditiis et quos
+      sint corrupti, a vel atque, nulla recusandae natus voluptatibus quisquam
+      laudantium architecto facilis quae! Quae doloribus esse asperiores et
+      quisquam distinctio tempore illo. Quae, aspernatur commodi! Sed totam
+      quidem dolorem unde laudantium assumenda quod explicabo temporibus autem
+      vero dicta cupiditate maxime odio beatae nihil provident delectus minima
+      obcaecati, voluptates aut placeat? Perspiciatis minima error suscipit!
+      Veniam, ut. Voluptates mollitia explicabo dolor beatae deleniti ipsa iste
+      eos optio aperiam! Expedita, tempore.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque deleniti
+      aliquid eius atque unde, accusantium placeat, reiciendis quam delectus,
+      expedita nostrum quis. Illum optio, velit modi dolor blanditiis et quos
+      sint corrupti, a vel atque, nulla recusandae natus voluptatibus quisquam
+      laudantium architecto facilis quae! Quae doloribus esse asperiores et
+      quisquam distinctio tempore illo. Quae, aspernatur commodi! Sed totam
+      quidem dolorem unde laudantium assumenda quod explicabo temporibus autem
+      vero dicta cupiditate maxime odio beatae nihil provident delectus minima
+      obcaecati, voluptates aut placeat? Perspiciatis minima error suscipit!
+      Veniam, ut. Voluptates mollitia explicabo dolor beatae deleniti ipsa iste
+      eos optio aperiam! Expedita, tempore.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
