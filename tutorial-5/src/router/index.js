8dca2ce36979ae54a691a3e5817ec3e7c0db37aa
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-07 12:29:13 +0200

    404 Pages & Redirects

--- tutorial-5/src/router/index.js
+++ tutorial-5/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
+import NotFound from "../views/NotFound.vue";
 import JobsView from "../views//jobs/JobsView.vue";
 import JobDetailsView from "../views/jobs/JobDetailsView.vue";
 
@@ -25,6 +26,17 @@ const routes = [
     name: "jobDetails",
     component: JobDetailsView,
     props: true,
+  },
+  // redirect
+  {
+    path: "/all-jobs",
+    redirect: "/jobs",
+  },
+  // catchall 404
+  {
+    path: "/:catchAll(.*)",
+    name: "notFound",
+    component: NotFound,
   },
 ];
 
