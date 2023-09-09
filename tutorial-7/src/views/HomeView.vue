f049cd194d5e1a8c3f24a7b9a9bcc8fbc2f64f86
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-09 14:53:36 +0200

    Using watch and watchEffect

--- tutorial-7/src/views/HomeView.vue
+++ tutorial-7/src/views/HomeView.vue
@@ -4,11 +4,12 @@
     <input type="text" v-model="search" />
     <p>search term - {{ search }}</p>
     <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    <button @click="handleClick">stop watching</button>
   </div>
 </template>
 
 <script>
-import { computed, ref } from "vue";
+import { computed, ref, watch, watchEffect } from "vue";
 
 export default {
   name: "HomeView",
@@ -16,12 +17,25 @@ export default {
     const search = ref("");
     const names = ref(["mario", "yoshi", "luigi", "toad", "bowser", "koopa"]);
 
+    const stopWatch = watch(search, () => {
+      console.log("watch function run");
+    });
+
+    const stopEffect = watchEffect(() => {
+      console.log("watchEffect function run", search.value);
+    });
+
     const matchingNames = computed(() => {
       // return ["a", "b", "c"];
       return names.value.filter((name) => name.includes(search.value));
     });
 
-    return { names, search, matchingNames };
+    const handleClick = () => {
+      stopWatch();
+      stopEffect();
+    };
+
+    return { names, search, matchingNames, handleClick };
   },
 };
 </script>
