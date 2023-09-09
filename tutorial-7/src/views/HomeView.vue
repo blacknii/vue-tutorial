40f54a73a1c7aa57731b267ed0bccd224425947c
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-09 13:48:57 +0200

    Composition API Basics

--- tutorial-7/src/views/HomeView.vue
+++ tutorial-7/src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    Home
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "HomeView",
+  setup() {
+    const name = "mario";
+    const age = 30;
+
+    const handleClick = () => {
+      console.log("you clicked me");
+    };
+
+    return {
+      name,
+      age,
+      handleClick,
+    };
+  },
 };
 </script>
