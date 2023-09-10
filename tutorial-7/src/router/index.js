340a4593f682eaf4582e3e16bed9e2605bb185e5
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-10 22:10:29 +0200

    Intro to Real-Time Listeners

--- tutorial-7/src/router/index.js
+++ tutorial-7/src/router/index.js
@@ -1,36 +1,42 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Details from '../views/Details.vue'
-import Create from '../views/Create.vue'
-import Tag from '../views/Tag.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Details from "../views/Details.vue";
+import Create from "../views/Create.vue";
+import Tag from "../views/Tag.vue";
+import RealTime from "../views/RealTime.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/posts/:id',
-    name: 'Details',
+    path: "/posts/:id",
+    name: "Details",
     component: Details,
-    props: true
+    props: true,
   },
   {
-    path: '/create',
-    name: 'Create',
-    component: Create
+    path: "/create",
+    name: "Create",
+    component: Create,
   },
   {
-    path: '/tags/:tag',
-    name: 'Tag',
-    component: Tag
-  }
-]
+    path: "/tags/:tag",
+    name: "Tag",
+    component: Tag,
+  },
+  {
+    path: "/realtime",
+    name: "RealTime",
+    component: RealTime,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
