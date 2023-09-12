74ed8a63c1a24e42c86cc23114780eed2f03eaeb
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-12 14:53:53 +0200

    New Chat Form

--- tutorial-8/src/views/Chatroom.vue
+++ tutorial-8/src/views/Chatroom.vue
@@ -1,28 +1,29 @@
 <template>
   <div class="container">
     <Navbar />
+    <NewChatForm />
   </div>
 </template>
 
 <script>
+import NewChatForm from "../components/NewChatForm.vue";
 import Navbar from "../components/Navbar.vue";
-import getUser from "@/composables/getUser";
+import getUser from "../composables/getUser";
 import { watch } from "vue";
 import { useRouter } from "vue-router";
 
 export default {
-  components: { Navbar },
+  components: { Navbar, NewChatForm },
   setup() {
     const router = useRouter();
     const { user } = getUser();
 
+    // if the user value is ever null, redirect to welcome screen
     watch(user, () => {
       if (!user.value) {
         router.push({ name: "Welcome" });
       }
     });
-
-    return { user };
   },
 };
 </script>
