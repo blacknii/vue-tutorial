9d674502a26e4904bf6ea6ede6146333445e10a1
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-12 15:35:19 +0200

    Final Route Guard

--- tutorial-8/src/router/index.js
+++ tutorial-8/src/router/index.js
@@ -1,15 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Welcome from "../views/Welcome.vue";
 import Chatroom from "../views/Chatroom.vue";
-import { projectAuth } from "@/firebase/config";
+import { projectAuth } from "../firebase/config";
 
-// auth guard
+// auth guards
 const requireAuth = (to, from, next) => {
   let user = projectAuth.currentUser;
   if (!user) {
     next({ name: "Welcome" });
+  } else {
+    next();
   }
-  next();
+};
+
+const requireNoAuth = (to, from, next) => {
+  let user = projectAuth.currentUser;
+  if (user) {
+    next({ name: "Chatroom" });
+  } else {
+    next();
+  }
 };
 
 const routes = [
@@ -17,6 +27,7 @@ const routes = [
     path: "/",
     name: "Welcome",
     component: Welcome,
+    beforeEnter: requireNoAuth,
   },
   {
     path: "/chatroom",
