a21b18bc3af20e83097cd915e184e468a1bc2b29
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-14 21:23:26 +0200

    .

---
+++ project-1/data/notes.js
@@ -0,0 +1,15 @@
+const db = [
+  { id: 1, content: 'Write a haiku about the sunset over the ocean.' },
+  { id: 2, content: 'Recall your favorite childhood memory and describe it in detail.' },
+  { id: 3, content: 'Share a secret recipe for the best chocolate chip cookies.' },
+  {
+    id: 4,
+    content: 'Write a short story set in a post-apocalyptic world where technology has vanished.'
+  },
+  {
+    id: 5,
+    content: 'List five things on your bucket list and why you want to experience them.'
+  }
+]
+
+export default db
--- project-1/src/views/NotesView.vue
+++ project-1/src/views/NotesView.vue
@@ -2,4 +2,14 @@
   <h1>This is an about page</h1>
 </template>
 
+<script>
+import db from '../../data/notes'
+
+export default {
+  setup() {
+    console.log(db)
+  }
+}
+</script>
+
 <style></style>
