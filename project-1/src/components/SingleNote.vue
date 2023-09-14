2d87e55c5ba6e02366a9e9ea683266f319df7f01
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-14 21:57:01 +0200

    .

---
+++ project-1/src/components/SingleNote.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="note">
+    <p>{{ note.content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['note'],
+  setup() {
+    return {}
+  }
+}
+</script>
+<style scoped>
+.note {
+  background-color: #2c3e50;
+  padding: 1.2rem;
+  border-radius: 10px;
+  width: 300px;
+  height: 140px;
+  overflow: auto;
+}
+
+.note::-webkit-scrollbar {
+  width: 0;
+}
+</style>
--- project-1/src/views/NotesView.vue
+++ project-1/src/views/NotesView.vue
@@ -1,15 +1,31 @@
 <template>
-  <h1>This is an about page</h1>
+  <h1>This is an notes page</h1>
+  <!-- <p v-for="note in notes" :key="note.id">{{ note.content }}</p> -->
+  <div class="notes">
+    <SingleNote v-for="note in notes" :key="note.id" :note="note" />
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 import db from '../../data/notes'
+import SingleNote from '../components/SingleNote.vue'
 
 export default {
+  components: { SingleNote },
   setup() {
-    console.log(db)
+    const notes = ref(db)
+
+    return { notes }
   }
 }
 </script>
 
-<style></style>
+<style>
+.notes {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 1rem;
+}
+</style>
