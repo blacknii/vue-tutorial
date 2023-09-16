f690badf312d99913790b343f52c61e1ed84c188
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-16 14:23:55 +0200

    .

--- project-3/src/views/HomeView.vue
+++ project-3/src/views/HomeView.vue
@@ -1,6 +1,12 @@
 <template>
   <main>
     <h1>This is an home page</h1>
+    <div>
+      <q-chip icon="event">Add to calendar</q-chip>
+      <q-chip icon="bookmark">Bookmark</q-chip>
+      <q-chip icon="alarm" label="Set alarm" />
+      <q-chip class="glossy" icon="directions">Get directions</q-chip>
+    </div>
   </main>
 </template>
 
