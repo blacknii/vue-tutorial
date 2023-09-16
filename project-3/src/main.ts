0af33ed317920dd2cb585bf9009239e973b1088b
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-16 12:08:12 +0200

    .

--- project-3/src/main.ts
+++ project-3/src/main.ts
@@ -1,11 +1,23 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import { Quasar } from "quasar";
 
-app.use(router)
+// Import icon libraries
+import "@quasar/extras/roboto-font/roboto-font.css";
+import "@quasar/extras/material-icons/material-icons.css";
+import "@quasar/extras/fontawesome-v6/fontawesome-v6.css";
 
-app.mount('#app')
+// Import Quasar css
+import "quasar/src/css/index.sass";
+
+const app = createApp(App).use(Quasar, {
+  plugins: {},
+});
+
+app.use(router);
+
+app.mount("#app");
