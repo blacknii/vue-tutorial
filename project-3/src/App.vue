429733e54ab9109d417c783413a28a58da905f61
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-16 13:02:18 +0200

    .

--- project-3/src/App.vue
+++ project-3/src/App.vue
@@ -1,9 +1,38 @@
 <template>
-  <RouterView />
+  <q-layout view="lHh lpr lFf" container style="min-height: 100vh">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn flat round dense icon="menu" class="q-mr-sm" />
+        <q-avatar>
+          <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+        </q-avatar>
+
+        <q-toolbar-title>Quasar Framework</q-toolbar-title>
+
+        <q-btn flat round dense icon="whatshot" />
+      </q-toolbar>
+    </q-header>
+
+    <q-footer elevated>
+      <q-toolbar>
+        <q-toolbar-title>Footer</q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+    <q-page-container>
+      <RouterView />
+    </q-page-container>
+  </q-layout>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import { RouterView } from "vue-router";
+
+export default {
+  setup(props) {
+    console.log("test");
+  },
+};
 </script>
 
 <style scoped></style>
