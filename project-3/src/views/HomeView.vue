4da6446035d37b69eaf46a58217dda205d1145bf
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-16 12:20:24 +0200

    .

--- project-3/src/views/HomeView.vue
+++ project-3/src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>This is an home page</h1>
   </main>
 </template>
+
+<script setup lang="ts"></script>
