95fbe0dfb97160719685b16a08f06bf849bd9b3b
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-18 11:29:02 +0200

    .

--- project-3/src/router/index.ts
+++ project-3/src/router/index.ts
@@ -1,32 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import AboutView from "../views/AboutView.vue";
-import ProfileView from "../views/ProfileView.vue";
+
 import CharactersView from "../views/CharactersView.vue";
+import ProfileView from "../views/ProfileView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: AboutView,
+      name: "characters",
+      component: CharactersView,
     },
     {
       path: "/profile/:id",
       name: "profile",
       component: ProfileView,
       props: true,
-    },
-    {
-      path: "/characters",
-      name: "characters",
-      component: CharactersView,
     },
   ],
 });
